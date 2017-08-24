d55ce4953c77ab6878dd22405aae0d9deaffe598
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-08-24 22:02:19 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,8 +18,9 @@ echo "DOCKER-ENTRYPOINT >> running as user: ${RUNAS}"
 #
 if [ -n "$KUBERNETES_CA_BASE64" ]
 then
-  echo $KUBERNETES_CA_BASE64 | openssl enc -base64 -d > /tmp/kube-ca.crt
+  echo $KUBERNETES_CA_BASE64 | base64 --decode > /tmp/kube-ca.crt
   echo "DOCKER-ENTRYPOINT >> KUBERNETES_CA_BASE64 ENV VAR > importing kubernetes ca certificate to java keystore."
+  cat /tmp/kube-ca.crt
   keytool -importcert -keystore /etc/ssl/certs/java/cacerts -alias kubernetes -file /tmp/kube-ca.crt -storepass changeit -noprompt
 else
   echo "DOCKER-ENTRYPOINT >> KUBERNETES_CA_BASE64 ENV VAR > not set. SKIPPING importing of kubernetes ca certificate."
