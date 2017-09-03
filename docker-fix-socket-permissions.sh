cc4140beb619c1bc246796e83c2f7314815280d2
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-09-03 22:09:19 +0200

    Update docker-fix-socket-permissions.sh

--- docker-fix-socket-permissions.sh
+++ docker-fix-socket-permissions.sh
@@ -8,24 +8,20 @@ set -e
 
 if [ -S /var/run/docker.sock ]
 then
+  # WARN: We could chgroup the socket BUT the chgrp will reach up to the docker host and render docker unusable or make unsafe on host!
+  #       Therefore we try to change the GID of the existing docker group inside the container, so that the IDs match
   JENKINS_USER="jenkins"
   echo "DOCKER-ENTRYPOINT >> DOCKER-SOCK-FIX: /var/run/docker.sock does exist. Applying GID fix."
   DOCKER_SOCK_GROUP=$(ls -lap /var/run/docker.sock  | awk '{ print $4 }')
   echo "DOCKER-ENTRYPOINT >> DOCKER-SOCK-FIX: docker.sock owned by group '${DOCKER_SOCK_GROUP}'"
-  re='^[0-9]+$'
-  if ! [[ $DOCKER_SOCK_GROUP =~ $re ]] ; then
-    # GROUP IS NOT NUMERIC => GROUP EXISTS INSIDE CONTAINER
-    if groups $JENKINS_USER | grep &>/dev/null "\b${DOCKER_SOCK_GROUP}\b"; then
-      echo "DOCKER-ENTRYPOINT >> DOCKER-SOCK-FIX: ${JENKINS_USER} is already part of the group. Skipping."
-    else
-      echo "DOCKER-ENTRYPOINT >> DOCKER-SOCK-FIX: ${JENKINS_USER} is not part of the group. Will be added."
-      usermod -aG $DOCKER_SOCK_GROUP $JENKINS_USER
-    fi
+  if [[ $DOCKER_SOCK_GROUP =~ ^-?[0-9]+$ ]]
+  then
+    # GROUP IS NUMERIC => no group that already has this id assigned
+    groupmod -g $DOCKER_SOCK_GROUP docker
+    echo "DOCKER-ENTRYPOINT >> DOCKER-SOCK-FIX: changing existing docker group to new GID"
   else
-    # GROUP IS NUMERIC => GROUP DOES NOT EXISTS INSIDE CONTAINER
-    echo "DOCKER-ENTRYPOINT >> DOCKER-SOCK-FIX: ${JENKINS_USER} is not part of the group. Group will be created. User will be added."
-    groupadd --gid $DOCKER_SOCK_GROUP g_$DOCKER_SOCK_GROUP
-    usermod -aG g_$DOCKER_SOCK_GROUP $JENKINS_USER
+    # GROUP IS NOT NUMERIC => some group exists with that id
+    echo "DOCKER-ENTRYPOINT >> DOCKER-SOCK-FIX: skipping. already a group with that id!"
   fi
 else
   echo "DOCKER-ENTRYPOINT >> DOCKER-SOCK-FIX: /var/run/docker.sock does not exist. Skipping GID fix."
